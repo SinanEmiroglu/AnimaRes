760636d1e1924dec4b837982f588239e18440ca5
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2020-11-07 14:06:20 +0200

    Imported DoTween & Added SceneLoader Script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AnimaRes
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void Load(int sceneIndex)
+        {
+            SceneManager.sceneLoaded += SceneLoadedHandler;
+            SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+        }
+
+        private void SceneLoadedHandler(Scene scene, LoadSceneMode mode)
+        {
+            MoveAfterLoad();
+            SceneManager.sceneLoaded -= SceneLoadedHandler;
+        }
+
+        private void MoveAfterLoad()
+        {
+        }
+    }
+}
